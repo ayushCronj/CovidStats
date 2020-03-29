26ff10f5dd61499566dc49267dfce2eb505886e3
Ayush Goel <ayush@cronj.com>
2020-03-29 19:25:12 +0530

    graphs added

--- src/country.js
+++ src/country.js
@@ -10,11 +10,60 @@ import {
 } from "@ant-design/icons";
 import { HomeFilled } from "@ant-design/icons";
 import { Redirect } from "react-router-dom";
+import ReactApexChart from "react-apexcharts";
 
 export class Country extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      series1: [],
+      options1: {},
+      series2: [],
+      options2: {},
+      series: [
+        {
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      options: {
+        chart: {
+          height: 350,
+          type: "line",
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: "straight"
+        },
+        title: {
+          text: "Product Trends by Month",
+          align: "left"
+        },
+        grid: {
+          row: {
+            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+            opacity: 0.5
+          }
+        },
+        xaxis: {
+          categories: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep"
+          ]
+        }
+      },
       data: [],
       countries: [],
       columns: [
@@ -77,8 +126,142 @@ export class Country extends React.Component {
           some = "Korea, South";
         }
         if (data[some || this.props.location.state.country]) {
+          let data1 = data[some || this.props.location.state.country];
+          let seriesData = [];
+          data1.forEach((element, index) => {
+            if (index % 2 == 0 && index > 40) {
+              seriesData.push(element.confirmed);
+            }
+          });
+          let optionData = [];
+          data1.forEach((element, index) => {
+            if (index % 2 == 0 && index > 40) {
+              optionData.push(element.date);
+            }
+          });
+
+          let seriesData2 = [];
+          data1.forEach((element, index) => {
+            if (index % 2 == 0 && index > 40) {
+              seriesData2.push(element.deaths);
+            }
+          });
+
+          let seriesData3 = [];
+          data1.forEach((element, index) => {
+            if (index % 2 == 0 && index > 40) {
+              console.log(element);
+              seriesData3.push(element.recovered);
+            }
+          });
+
           this.setState({
-            data: data[some || this.props.location.state.country].reverse()
+            data: data[some || this.props.location.state.country].reverse(),
+            series: [
+              {
+                name: "Confirmed Cases",
+                data: seriesData
+              }
+            ],
+            options: {
+              chart: {
+                height: 350,
+                type: "line",
+                zoom: {
+                  enabled: false
+                }
+              },
+              dataLabels: {
+                enabled: false
+              },
+              stroke: {
+                curve: "straight"
+              },
+              title: {
+                text: "Confirmed Cases",
+                align: "left"
+              },
+              grid: {
+                row: {
+                  colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+                  opacity: 0.5
+                }
+              },
+              xaxis: {
+                categories: optionData
+              }
+            },
+
+            series1: [
+              {
+                name: "Deaths",
+                data: seriesData2
+              }
+            ],
+            options1: {
+              chart: {
+                height: 350,
+                type: "line",
+                zoom: {
+                  enabled: false
+                }
+              },
+              colors: ["#FF0000"],
+              dataLabels: {
+                enabled: false
+              },
+              stroke: {
+                curve: "straight"
+              },
+              title: {
+                text: "Deaths",
+                align: "left"
+              },
+              grid: {
+                row: {
+                  colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+                  opacity: 0.5
+                }
+              },
+              xaxis: {
+                categories: optionData
+              }
+            },
+            series2: [
+              {
+                name: "Recovered",
+                data: seriesData3
+              }
+            ],
+            options2: {
+              chart: {
+                height: 350,
+                type: "line",
+                zoom: {
+                  enabled: false
+                }
+              },
+              colors: ["#00FF00"],
+              dataLabels: {
+                enabled: false
+              },
+              stroke: {
+                curve: "straight"
+              },
+              title: {
+                text: "Recovered",
+                align: "left"
+              },
+              grid: {
+                row: {
+                  colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+                  opacity: 0.5
+                }
+              },
+              xaxis: {
+                categories: optionData
+              }
+            }
           });
         } else {
           this.setState({
@@ -118,7 +301,7 @@ export class Country extends React.Component {
           />
         </div>
 
-        <div className="mainCard">
+        <div className="mainCard1">
           <Card className="card">
             {this.props.location && this.props.location.state ? (
               <div>Total Cases - {this.props.location.state.cases} </div>
@@ -136,6 +319,38 @@ export class Country extends React.Component {
               </div>
             ) : null}
           </Card>
+        </div>
+
+        <div className="mainCard">
+          <div className="chart">
+            <ReactApexChart
+              options={this.state.options}
+              series={this.state.series}
+              type="line"
+              height={250}
+              // width={645}
+            />
+          </div>
+
+          <div className="chart">
+            <ReactApexChart
+              options={this.state.options1}
+              series={this.state.series1}
+              type="line"
+              height={250}
+              // width={645}
+            />
+          </div>
+
+          <div className="chart">
+            <ReactApexChart
+              options={this.state.options2}
+              series={this.state.series2}
+              type="line"
+              height={250}
+              // width={645}
+            />
+          </div>
         </div>
 
         <Table
